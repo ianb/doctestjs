818b9bb910d60399696ba30a5dee1bf775277ed7
Ian Bicking <ianb@colorstudy.com>
2010-09-11 13:24:58 -0500

    updates to examples and rebuild

--- rebuild.py
+++ rebuild.py
@@ -7,13 +7,14 @@ here = os.path.dirname(os.path.abspath(__file__))
 os.chdir(here)
 
 template = open('index_template.html').read()
-content = open('docs/index.html').read()
-
-match = re.search(r'<body.*?>', content, re.I)
-content = content[match.end():]
-match = re.search(r'</body>', content, re.I)
-content = content[:match.start()]
-page = template.replace('__BODY__', content)
-fp = open('index.html', 'w')
-fp.write(page)
-fp.close();
+pages = ['index.html', 'examples.html']
+for name in pages:
+    content = open('docs/' + name).read()
+    match = re.search(r'<body.*?>', content, re.I)
+    content = content[match.end():]
+    match = re.search(r'</body>', content, re.I)
+    content = content[:match.start()]
+    page = template.replace('__BODY__', content)
+    fp = open(name, 'w')
+    fp.write(page)
+    fp.close();
