70e441610bfbccfb0a6dab6480579adad2a3bff4
Ian Bicking <ianb@colorstudy.com>
2010-10-10 22:02:47 -0500

    Refactor the documentation

--- .resources/toc.js
+++ .resources/toc.js
@@ -1,7 +1,7 @@
 function contentsOnLoad() {
     var dest = document.getElementById('contents');
     var toc = document.createElement('ul');
-    var els = document.getElementsByTagName('h2');
+    var els = document.getElementsByTagName('h3');
     for (var i=0; i<els.length; i++) {
         var el = els[i];
         var name = 'header-'+(i+1);
