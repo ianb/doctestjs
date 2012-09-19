f78228591872805d1a0fdb401a4c7f6190c8218b
Ian Bicking <ianb@colorstudy.com>
2012-09-19 01:30:21 -0500

    deployment 0cba566

--- .remote-repo/examples/node-example.js
+++
@@ -1,6 +0,0 @@
-var doctest = require('./doctest.js');
-
-var runner = new doctest.Runner({Reporter: doctest.ConsoleReporter});
-var parser = new doctest.TextParser.fromFile(runner, './examples-2.js');
-parser.parse();
-runner.run();
---
+++ example.js
@@ -0,0 +1,32 @@
+
+
+// sorted keys
+// wildcards
+// console output
+// mocker
+// async
+// mocker + async
+
+// You don't forget to write tests
+// You err on overtesting instead of undertesting
+//   hidden attributes
+//   hidden this
+
+
+/*
+// Some setup
+$ base = doctest.params.base
+>   || 'https://www.googleapis.com/freebase/v1'
+$ print(base);
+...
+
+$.ajax({
+  url: base + '/search',
+  data: {query: 'nirvana'},
+  success: Spy('search', {wait: true})
+});
+
+/* => {...} */
+
+// features:
+//
