637879042ca856d6812c4309534151dd9eeacf14
Ian Bicking <ianb@colorstudy.com>
2012-09-19 01:38:34 -0500

    deployment f82b256-dirty

--- resources/toc.js
+++ resources/toc.js
@@ -1,13 +1,22 @@
 function contentsOnLoad() {
     var dest = document.getElementById('contents');
-    var toc = document.createElement('ul');
-    var els = document.getElementsByTagName('h3');
+    var toc = [document.createElement('ul')];
+    dest.appendChild(toc[0]);
+    var els = document.querySelectorAll('h3, h4, h5, h6');
     for (var i=0; i<els.length; i++) {
         var el = els[i];
+        var elDepth = ['H3', 'H4', 'H5', 'H6'].indexOf(el.tagName);
+        while (elDepth < toc.length-1) {
+          toc.splice(toc.length-1, 1);
+        }
+        while (elDepth >= toc.length) {
+          var ul = document.createElement('ul');
+          var container = document.createElement('li');
+          container.appendChild(ul);
+          toc[toc.length-1].appendChild(container);
+          toc.push(ul);
+        }
         var name = 'header-'+(i+1);
-        var marker = document.createElement('a');
-        marker.setAttribute('name', name);
-        el.parentNode.insertBefore(marker, el);
         el.setAttribute('id', name);
         var li = document.createElement('li');
         var anchor = document.createElement('a');
@@ -19,13 +28,12 @@ function contentsOnLoad() {
         }
         li.appendChild(anchor);
         anchor.innerHTML = el.innerHTML;
-        toc.appendChild(li);
+        toc[toc.length-1].appendChild(li);
     }
-    dest.appendChild(toc);
+    // Re-scroll:
     if (location.hash) {
       location.hash = location.hash;
     }
 }
 
 window.onload = contentsOnLoad;
-
